fb498483fab4d594a27828e9015bc3567c45b01b
Arshad-Nazeer <arshadnhusain@gmail.com>
2026-07-22 22:38:00 +0530

    Initial commit

---
+++ src/IntroductionOutput.java
@@ -0,0 +1,27 @@
+public class IntroductionOutput {
+    public static void main(String[] args) {
+        // Java is a high-level, object-oriented, platform-independent programming language developed by Sun Microsystems (now owned by Oracle). It is designed to let developers write code once and run it anywhere using the Java Virtual Machine (JVM)
+
+        // The Java Virtual Machine (JVM) is a virtual machine that executes Java bytecode and enables Java programs to run on different operating systems without modification.
+        // JRE = JVM + Java Class Libraries
+        // Run Java programs
+        // Cannot compile Java source code.
+
+        // The Java Runtime Environment (JRE) is a software package that provides everything needed to run Java applications, including the JVM and the required Java libraries.
+        // JDK = JRE + Development Tools
+        // javac – Compiles .java files into .class files.
+        // java – Runs Java programs.
+        // javadoc – Generates documentation.
+        // jar – Creates Java archive files.
+
+        //output and jump to next line
+        System.out.println("Hello World!");
+        //output with newline character
+        System.out.print("I like pizza\n");
+
+        /*
+        This is a multi-line comment
+         */
+        // sout:- shortcut to write "System.out.println"
+    }
+}
---
+++ src/variables.java
@@ -0,0 +1,62 @@
+public class variables {
+    public static void main(String[] args) {
+        // variable:-
+        // a reusable container for a value
+        // a variable behaves as if it was the value it contains
+
+        // primitive :- simple value stored directly in memory (stack)
+        // byte, short, int, long, float, double, char, boolean
+        // Reference :- memory address (stack) that points to the (heap)
+        // String, Arrays, Classes (Objects), Interfaces, Enums, Wrapper Classes, Records
+
+        // creating a variable :-
+        // declaration
+        // assignment
+
+        int age;
+        age=21;
+        System.out.println(age);
+
+        int year=2025;
+        System.out.println("The year is "+year);
+
+        double price=19, gpa=3.5, temperature=-12.5;
+        System.out.println("$ "+price);
+        System.out.println(gpa);
+        System.out.println(temperature);
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+        System.out.println(grade);
+        System.out.println(symbol);
+        System.out.println(currency);
+
+        boolean isStudent = true;
+        boolean isForSale = false;
+        System.out.println(isForSale);
+
+        if(isStudent==true){
+            System.out.println("You are a Student");
+        }else{
+            System.out.println("You are not a Student");
+        }
+
+        String name="Arshad"; // a series of characters
+        System.out.println("Hello! "+name);
+        String food = "pizza";
+        System.out.println("Your favorite food is "+food);
+
+        System.out.println("You are "+age+" years old");
+        System.out.println("Your gpa is: "+gpa);
+
+        String color="red", car="Mustang";
+        System.out.println("Your choice is a "+color+" "+year+" "+car);
+        System.out.println("The price is: "+currency+price);
+
+        if(isForSale)
+            System.out.println("The "+car+" is for sale");
+        else
+            System.out.println("The "+car+" is not for sale");
+    }
+}
